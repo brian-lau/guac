446d3d93d339859ea3fb9b85e98dd2ebf18cd2bb
dallascard <dcard@andrew.cmu.edu>
2015-09-26 16:10:48 -0400

    changed matrix to dataframe for ease

---
+++ core/experiment/summarize_results.py
@@ -0,0 +1,46 @@
+import os
+from optparse import OptionParser
+import glob
+
+import numpy as np
+import pandas as pd
+
+from ..util import defines
+from ..util import file_handling as fh
+
+
+
+
+def main():
+
+    usage = "%prog exp_dir"
+    parser = OptionParser(usage=usage)
+
+    (options, args) = parser.parse_args()
+    exp_dir = args[0]
+
+    results = pd.DataFrame(columns=('masked', 'test', 'valid', 'dir'))
+
+    run_dirs = glob.glob(os.path.join(exp_dir, 'test_fold_0', 'bayes*'))
+    for i, dir in enumerate(run_dirs):
+        results_dir = os.path.join(dir, 'results')
+        test_file = fh.make_filename(results_dir, 'test_macro_f1', 'csv')
+        valid_file = fh.make_filename(results_dir, 'valid_cv_macro_f1', 'csv')
+        masked_valid_file = fh.make_filename(results_dir, 'masked_valid_cv_macro_f1', 'csv')
+
+        test = pd.read_csv(test_file, header=False, index_col=0)
+        valid = pd.read_csv(valid_file, header=False, index_col=0)
+        masked_valid = pd.read_csv(masked_valid_file, header=False, index_col=0)
+
+        results.loc[i, 'masked'] = masked_valid['overall'].mean()
+        results.loc[i, 'test'] = test['overall'].mean()
+        results.loc[i, 'valid'] = valid['overall'].mean()
+        results.loc[i, 'dir'] = fh.get_basename(dir)
+
+    order = np.argsort(results['masked'])
+
+    print results.loc[order, :]
+
+
+if __name__ == '__main__':
+    main()
